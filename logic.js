646a8c419122cc5ed9ed695067d16a1683155f05
Masin271 <masinmasin271@gmail.com>
2024-10-12 00:12:52 +0330

    I'm about to give up

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-let firstNum;
+let num = " ";
 let secondNum;
 let operator;
 let display = document.querySelector(".display");
@@ -12,59 +12,94 @@ let btnSeven = document.querySelector(".seven");
 let btnEight = document.querySelector(".eight");
 let btnNine = document.querySelector(".nine");
 let btnClear = document.querySelector(".clear");
+let btnADD = document.querySelector(".add");
+let btnSubtract = document.querySelector(".subtract");
+let btnMultiply = document.querySelector(".multiply");
+let btnDivide = document.querySelector(".divide");
+let btnEqual = document.querySelector(".equal");
+let btnZero = document.querySelector(".zero");
+let btnFloat = document.querySelector(".float");
+btnFloat.addEventListener("click", () => {
+  display.textContent += ".";
+  num += ".";
+})
+btnZero.addEventListener("click", () => {
+  display.textContent += "0";
+  num += "0";
+})
 btnOne.addEventListener("click", () => {
   display.textContent += "1";
+  num += "1";
 });
 btnTwo.addEventListener("click", () => {
   display.textContent += "2";
+  num += "2";
 });
 btnThree.addEventListener("click", () => {
   display.textContent += "3";
+  num += "3";
 });
 btnFour.addEventListener("click", () => {
   display.textContent += "4";
+  num += "4";
 });
 btnFive.addEventListener("click", () => {
   display.textContent += "5";
+  num += "5";
 });
 btnSix.addEventListener("click", () => {
   display.textContent += "6";
+  num += "6";
 });
 btnSeven.addEventListener("click", () => {
   display.textContent += "7";
+  num += "7";
 });
 btnEight.addEventListener("click", () => {
   display.textContent += "8";
+  num += "8";
 });
 btnNine.addEventListener("click", () => {
   display.textContent += "9";
+  num += "9";
+});
+btnADD.addEventListener("click", () => {
+  display.textContent = "+";
+  num += " + ";
+});
+btnSubtract.addEventListener("click", () => {
+  display.textContent = "-";
+  num += " - ";
+});
+btnMultiply.addEventListener("click", () => {
+  display.textContent = "*";
+  num += " * ";
+});
+btnDivide.addEventListener("click", () => {
+  display.textContent = "/";
+  num += " / ";
+});
+btnEqual.addEventListener("click", () => {
+  let condition = display.textContent.split("");
+  if (condition[condition.indexOf("+")] == "+") {
+    num = num.split(" + ");
+    display.textContent = Number(num[0]) + Number(num[1]);
+    return display.textContent;
+  } else if (condition[condition.indexOf("-")] == "-") {
+    num = num.split(" - ");
+    display.textContent = Number(num[0]) - Number(num[1]);
+    return display.textContent;
+  } else if (condition[condition.indexOf("*")] == "*") {
+    num = num.split(" * ");
+    display.textContent = Number(num[0]) * Number(num[1]);
+    return display.textContent;
+  } else if (condition[condition.indexOf("/")] == "/") {
+    num = num.split(" / ");
+    display.textContent = Number(num[0]) / Number(num[1]);
+    return display.textContent;
+  }
 });
 btnClear.addEventListener("click", () => {
-  display.textContent = " ";
+  display.textContent = "";
+  num = "";
 });
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-function operate(operator, num1, num2) {
-  if (operator == "+") {
-    add(num1, num2);
-  } else if (operator == "-") {
-    subtract(num1, num2);
-  } else if (operator == "*") {
-    multiply(num1, num2);
-  } else if (operator == "/") {
-    divide(num1, num2);
-  }
-}
